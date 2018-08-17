68920fc2ac3bbd5bbed3c7eab85287adb8bc4b15
Jose Manuel Rodriguez Calvo <jmr_calvo@outlook.com>
2018-08-18 01:14:08 +0200

    Set working
    the program now can add flights, and every flight must have a difference flight number to be able to be recorded in the program

--- core_program.py
+++ core_program.py
@@ -14,6 +14,7 @@ class plane():
      bags_allowed=0
      bag_price=0
      flight_number="NA"
+
      def create(self,so,des,dep,ar,fl,pr,ba,bp):
          self.source=so
          self.destination=des
@@ -23,19 +24,30 @@ class plane():
          self.bags_allowed=ba
          self.bag_price=bp
          self.flight_number=fl
+
      def print_plane(self):
         print(self.flight_number)
 
+     def __eq__(self,plane_o):
+         if isinstance(plane_o, plane):
+             return self.flight_number==plane_o.flight_number;
+         return NotImplemented
+     def __hash__(self):
+         return hash(self.flight_number)
 
 #********************methods for the program************************************
-def createeach(line):
+def createEach(plane_set,line):
     arr=line.split(",")
     plane_n=plane();
     plane_n.create(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7])
-    plane_n.print_plane()
+    plane_set.add(plane_n)
+    #plane_n.print_plane()
 
 #***************this is the first line of the main******************************
+plane_set=set()
 for line in sys.stdin:
     line = line.strip()
-    createeach(line)
+    createEach(plane_set,line)
     #print(line)
+for item in plane_set:
+    item.print_plane()
