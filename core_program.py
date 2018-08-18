58f38f9bc42900600f93a958e4fc1e9fea05875c
Jose Manuel Rodriguez Calvo <jmr_calvo@outlook.com>
2018-08-18 03:03:18 +0200

    delimiting the bags
    now the program will short the list limiting the bags that are requested

--- core_program.py
+++ core_program.py
@@ -5,7 +5,7 @@ import sys
 
 
 #*****************class for the flights*****************************************
-class plane():
+class Flights():
      source="NA"
      destination="NA"
      departure="NA"
@@ -20,34 +20,45 @@ class plane():
          self.destination=des
          self.departure=dep
          self.arrival=ar
-         self.price=pr
-         self.bags_allowed=ba
-         self.bag_price=bp
+         self.price=float(pr)
+         self.bags_allowed=int(ba)
+         self.bag_price=float(bp)
          self.flight_number=fl
 
      def print_plane(self):
         print(self.flight_number)
 
-     def __eq__(self,plane_o):
-         if isinstance(plane_o, plane):
-             return self.flight_number==plane_o.flight_number;
+     def __eq__(self,other):
+         if isinstance(other, Flights):
+             return self.flight_number==other.flight_number;
          return NotImplemented
      def __hash__(self):
          return hash(self.flight_number)
 
 #********************methods for the program************************************
-def createEach(plane_set,line):
+def createEach(flights_set,line):
     arr=line.split(",")
-    plane_n=plane();
-    plane_n.create(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7])
-    plane_set.add(plane_n)
-    #plane_n.print_plane()
+    flight_n=Flights();
+    flight_n.create(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7])
+    flights_set.add(flight_n)
+
+def starter(flights_set,n_bags=0):
+    availables_flights=set()
+    for n_flight in flights_set:
+        if n_flight.bags_allowed >= n_bags :
+            availables_flights.add(n_flight)
+
+    # for item in availables_flights:
+    #     item.print_plane()
 
 #***************this is the first line of the main******************************
-plane_set=set()
+flights_set=set()
 for line in sys.stdin:
     line = line.strip()
-    createEach(plane_set,line)
+    createEach(flights_set,line)
     #print(line)
-for item in plane_set:
+for item in flights_set:
     item.print_plane()
+
+print("\n hasta aqui es el primer set \n")
+starter(flights_set,1)
